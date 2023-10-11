eaff31641d59df5cae7ace7f0d4b18fe4a2dbccf
marksaunders342 <143830510+marksaunders342@users.noreply.github.com>
2023-10-11 11:20:19 +0100

    Create ClientPlaylistList.js

---
+++ Service/ClientPlaylistList.js
@@ -0,0 +1,176 @@
+const { Client } = require('pg')
+const types = require('pg').types
+const express = require('express')
+
+const healthgainzConfig = require('./HealthGainzConfig')
+
+const clientPlaylistSelectSQL = 'SELECT playlist.name, clientplaylist.* FROM clientplaylist JOIN playlist ON clientplaylist.playlistid = playlist.id'
+
+types.setTypeParser(types.builtins.INT8, (value) => {
+	return value == null ? null : parseInt(value)
+})
+
+types.setTypeParser(types.builtins.NUMERIC, (value) => {
+	return value == null ? null : parseFloat(value)
+})
+
+types.setTypeParser(types.builtins.INT4, (value) => {
+	return value == null ? null : parseInt(value)
+})
+
+types.setTypeParser(types.builtins.DATE, (value) => {
+	return value == null ? null : value.substring(0, 10)
+})
+
+const handleError = (response, message) => {
+    response.writeHead(409, {'Content-Type': 'text/plain'})
+    response.end(message)
+}
+
+const checkCredentials = async (request, roles, healthgainzClient) => {
+	let values = request.headers.authorization.split(':')
+	let result = await healthgainzClient.query('SELECT * FROM "user" WHERE emailaddress = $1 AND password = $2', values)
+	if (result.rows.length == 0) throw new Error('Login is not valid')
+	let userRoles = result.rows[0].roles
+    let permitted = roles.some((item) => userRoles.includes(item))
+	if (!permitted) throw new Error('Login does not have the required roles')
+}
+
+const doFilterQuery = async (sql, values, request, response) => {
+    let healthgainzClient = new Client(healthgainzConfig)
+    try {
+        await healthgainzClient.connect()
+        await checkCredentials(request, ['Administrator', 'Therapist', 'Client'], healthgainzClient)
+        let result = values.length ? await healthgainzClient.query(sql, values) : await healthgainzClient.query(sql)
+        response.writeHead(200, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify(result.rows))
+    }
+    catch (error) {
+        handleError(response, error.message)
+    }
+    finally {
+        await healthgainzClient.end()
+    }
+}
+
+const app = express()
+app.use(express.json())
+
+// Enable CORS for all methods
+app.use(function (request, response, next) {
+    response.header('Access-Control-Allow-Origin', '*')
+    response.header('Access-Control-Allow-Headers', '*')
+    next()
+})
+
+app.post('/createClientPlaylist', async (request, response) => {
+    let healthgainzClient = new Client(healthgainzConfig)
+    try {
+        await healthgainzClient.connect()
+		await checkCredentials(request, ['Administrator', 'Therapist'], healthgainzClient)
+        let result = await healthgainzClient.query('INSERT INTO clientplaylist VALUES (DEFAULT, $1, $2) RETURNING *', Object.values(request.body))
+		response.writeHead(200, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify(result.rows[0]))
+    }
+    catch (error) {
+        handleError(response, error.message)
+    }
+    finally {
+        await healthgainzClient.end()
+    }
+})
+
+app.post('/updateClientPlaylist', async (request, response) => {
+    let healthgainzClient = new Client(healthgainzConfig)
+    try {
+        await healthgainzClient.connect()
+		await checkCredentials(request, ['Administrator', 'Therapist'], healthgainzClient)
+        let result = await healthgainzClient.query('UPDATE clientplaylist SET clientid = $2, playlistid = $3 WHERE id = $1 RETURNING *', Object.values(request.body))
+		response.writeHead(200, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify(result.rows[0]))
+    }
+    catch (error) {
+        handleError(response, error.message)
+    }
+    finally {
+        await healthgainzClient.end()
+    }
+})
+
+app.get('/deleteClientPlaylist', async (request, response) => {
+    let healthgainzClient = new Client(healthgainzConfig)
+    try {
+        await healthgainzClient.connect()
+		await checkCredentials(request, ['Administrator', 'Therapist'], healthgainzClient)
+        await healthgainzClient.query('DELETE FROM clientplaylist WHERE id = $1', [request.query.id])
+        response.writeHead(200)
+        response.end()
+    }
+    catch (error) {
+        handleError(response, error.message)
+    }
+    finally {
+        await healthgainzClient.end()
+    }
+})
+
+app.get('/getClientPlaylistById', async (request, response) => {
+    let healthgainzClient = new Client(healthgainzConfig)
+    try {
+        await healthgainzClient.connect()
+		await checkCredentials(request, ['Administrator', 'Therapist', 'Client'], healthgainzClient)
+        let result = await healthgainzClient.query(clientPlaylistSelectSQL + ' WHERE clientplaylist.id = $1', [request.query.id])
+        if (result.rows.length == 0) throw new Error('ClientPlaylist not found')
+		else {
+			response.writeHead(200, {'Content-Type': 'application/json'})
+			response.end(JSON.stringify(result.rows[0]))
+		}
+    }
+    catch (error) {
+        handleError(response, error.message)
+    }
+    finally {
+        await healthgainzClient.end()
+    }
+})
+
+app.get('/getClientPlaylistsByClient', async (request, response) => {
+    let healthgainzClient = new Client(healthgainzConfig)
+    try {
+        await healthgainzClient.connect()
+		await checkCredentials(request, ['Administrator', 'Therapist', 'Client'], healthgainzClient)
+        let result = await healthgainzClient.query(clientPlaylistSelectSQL + ' WHERE clientplaylist.clientid = $1', [request.query.clientId])
+        response.writeHead(200, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify(result.rows))
+    }
+    catch (error) {
+        handleError(response, error.message)
+    }
+    finally {
+        await healthgainzClient.end()
+    }
+})
+
+app.get('/getClientPlaylistsByClientAndNameContains', (request, response) => {
+    let query = request.query
+    let value = query.value
+    if (!value) { handleError(response, 'Value required'); return }
+    let sql = clientPlaylistSelectSQL + ' WHERE clientplaylist.clientid = $1 AND playlist.name ILIKE $2'
+    doFilterQuery(sql, [query.clientId, '%' + value + '%'], request, response)
+})
+
+app.get('/getClientPlaylistsByClientAndNameEmpty', (request, response) => {
+    let sql = clientPlaylistSelectSQL + ' WHERE clientplaylist.clientid = $1 AND playlist.name IS NULL'
+    doFilterQuery(sql, [request.query.clientId], request, response)
+})
+
+app.get('/getClientPlaylistsByClientAndNameNotEmpty', (request, response) => {
+    let sql = clientPlaylistSelectSQL + ' WHERE clientplaylist.clientid = $1 AND playlist.name IS NOT NULL'
+    doFilterQuery(sql, [request.query.clientId], request, response)
+})
+
+app.listen(3009, () => {
+    console.log('Microservice \'HealthGainz:ClientPlaylistList\' running on port 3009')
+})
+
+module.exports = app
