af8160ba64e3ed9f0ab1708f8431eed2e182208d
marksaunders342 <143830510+marksaunders342@users.noreply.github.com>
2023-10-11 11:20:26 +0100

    Create HealthGainzConfig.js

---
+++ Service/HealthGainzConfig.js
@@ -0,0 +1,9 @@
+const healthgainzConfig = {
+    host: 'localhost',
+    port: '5432',
+    database: 'healthgainz',
+    user: 'postgres',
+    password: 'MwuKjaPre1'
+}
+
+export default healthgainzConfig
