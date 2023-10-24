84fabfc834e03e9768427b7581ace7e4952758b9
marksaunders342 <143830510+marksaunders342@users.noreply.github.com>
2023-10-24 11:07:33 +0100

    Update HealthGainzLibrary.js

--- Service/HealthGainzLibrary.js
+++ Service/HealthGainzLibrary.js
@@ -1,6 +1,6 @@
 // version 1
 
-export const healthgainzConfig = {
+exports.healthgainzConfig = {
     host: 'localhost',
     port: '5432',
     database: 'healthgainz',
@@ -8,7 +8,7 @@ export const healthgainzConfig = {
     password: 'MwuKjaPre1'
 }
 
-export const checkCredentials = async (request, roles, healthgainzClient) => {
+exports.checkCredentials = async (request, roles, healthgainzClient) => {
 	let values = request.headers.authorization.split(':')
 	let result = await healthgainzClient.query('SELECT * FROM "user" WHERE emailaddress = $1 AND password = $2', values)
 	if (result.rows.length == 0) throw new Error('Login is not valid')
@@ -17,7 +17,7 @@ export const checkCredentials = async (request, roles, healthgainzClient) => {
 	if (!permitted) throw new Error('Login does not have the required roles')
 }
 
-export const handleError = (response, message) => {
+exports.handleError = (response, message) => {
     response.writeHead(409, {'Content-Type': 'text/plain'})
     response.end(message)
 }
